39bf6d2d58ccc70585fc695c400807122701b0f6
GilangRamandan <gilangx288@gmail.com>
2026-05-20 15:37:13 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,9 @@
-// GilangStore Supabase config
-// Upload file ini di root GitHub/Cloudflare Pages, sejajar dengan index.html dan admin.html
+// Supabase config GilangStore
+// File ini harus sejajar dengan index.html dan admin.html
 window.SUPABASE_URL = "https://mijjgufgpzwzmrhnljmi.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pampndWZncHp3em1yaG5sam1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxODI5NjEsImV4cCI6MjA5NDc1ODk2MX0.426yzaQ_fr6s8Dk9jB_7gIBZP3lv8hKmu-UqlkcmzH4";
 
-// Biar kompatibel kalau ada file lama yang pakai nama config berbeda
-window.GILANGSTORE_SUPABASE_URL = window.SUPABASE_URL;
-window.GILANGSTORE_SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY;
+window.GILANGSTORE_CONFIG = {
+  SUPABASE_URL: window.SUPABASE_URL,
+  SUPABASE_ANON_KEY: window.SUPABASE_ANON_KEY
+};
