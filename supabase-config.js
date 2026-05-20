81a4845dc808a7415e42a5763f389a172b7d2354
GilangRamandan <gilangx288@gmail.com>
2026-05-20 14:19:56 +0700

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+// GilangStore Supabase Config
+// Pakai ANON PUBLIC KEY saja. Jangan pernah pakai service_role key di web.
+window.GILANGSTORE_SUPABASE_URL = "https://mijjgufgpzwzmrhnljmi.supabase.co";
+window.GILANGSTORE_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pampndWZncHp3em1yaG5sam1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxODI5NjEsImV4cCI6MjA5NDc1ODk2MX0.426yzaQ_fr6s8Dk9jB_7gIBZP3lv8hKmu-UqlkcmzH4";
