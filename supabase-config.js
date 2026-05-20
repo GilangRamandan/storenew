3a24d1da03de4f01e417c4565616df45770be1d4
GilangRamandan <gilangx288@gmail.com>
2026-05-20 14:56:05 +0700

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// Supabase config GilangStore
+window.SUPABASE_URL = "https://mijjgufgpzwzmrhnljmi.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pampndWZncHp3em1yaG5sam1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxODI5NjEsImV4cCI6MjA5NDc1ODk2MX0.426yzaQ_fr6s8Dk9jB_7gIBZP3lv8hKmu-UqlkcmzH4";
+window.GILANGSTORE_SUPABASE = {
+  url: window.SUPABASE_URL,
+  anonKey: window.SUPABASE_ANON_KEY
+};
